fd2f6562bad6f8dde15a0259ed7e9339e07cd08c
will-nuc <zhangxianglink@163.com>
2021-01-31 12:47:47 +0800

    svg vu vue 的单一引入

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,19 +1,22 @@
 <template>
-    <div class="nav">
-      <router-link to="/">money</router-link> |    
-      <router-link to="/lables">lables</router-link> |
-      <router-link to="/statistics">statistics</router-link>
-    </div>
+  <div class="nav">
+    <router-link to="/">
+      <svg>
+        <use xlink:href="#money"/>
+      </svg>
+      money
+    </router-link>
+    | <router-link to="/lables">lables</router-link> |
+    <router-link to="/statistics">statistics</router-link>
+  </div>
 </template>
 
 <script lang="ts">
-export default {
-    
-}
+export default {};
 </script>
 
 <style lang="scss" scoped>
-  .nav {
-    border: 1px solid green;
-  }
+.nav {
+  border: 1px solid green;
+}
 </style>
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -2,3 +2,16 @@ declare module '*.vue' {
   import Vue from 'vue'
   export default Vue
 }
+
+declare module '*.svg' {
+  {
+    const content: string;
+    export default content;
+  }
+}
+declare module '*.png'
+declare module '*.jpg'
+declare module '*.jpeg'
+declare module '*.gif'
+declare module '*.bmp'
+declare module '*.tiff'
